78a8ab3f44fc9796536f2835a828d3d29ec16e84
youssef <git@youssefazrak.com>
2018-11-04 17:16:39 +0000

    Cleaned the file

--- chap7_half_func.go
+++ chap7_half_func.go
@@ -2,11 +2,8 @@ package main
 
 import "fmt"
 
-//func sum(tot_sum []int) int {
-//}
-
 func halves(x int) (int, bool) {
-	half := x / 2 
+	half := x / 2
 	if x % 2 == 0 {
 		return half, true
 	} else {
